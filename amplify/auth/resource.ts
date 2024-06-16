55965b4045cc3918976d966370b6b1147ecfeb2a
Jonathan <jonathan.joe2745@gmail.com>
2024-06-16 11:59:02 +0800

    16 June 2024 - Bug Fixing and addition of delete i tem fucntionality

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,4 +1,19 @@
-// import { defineAuth } from '@aws-amplify/backend';
+// // import { defineAuth } from '@aws-amplify/backend';
+
+// // /**
+// //  * Define and configure your auth resource
+// //  * @see https://docs.amplify.aws/gen2/build-a-backend/auth
+// //  */
+// // export const auth = defineAuth({
+// //   loginWith: {
+// //     email: true,
+// //   },
+// // });
+
+
+// import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
+
+// import { defineAuth } from "@aws-amplify/backend"
 
 // /**
 //  * Define and configure your auth resource
@@ -8,25 +23,35 @@
 //   loginWith: {
 //     email: true,
 //   },
+// })
+
+// const schema = a.schema({
+//   Todo: a
+//     .model({
+//       content: a.string(),
+//       isDone: a.boolean()
+//     })
+//     .authorization(allow => [allow.owner()])
+// });
+
+// export type Schema = ClientSchema<typeof schema>;
+
+// export const data = defineData({
+//   schema,
+//   authorizationModes: {
+//     defaultAuthorizationMode: 'userPool'
+//   }
 // });
 
 
-import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
+import { defineAuth } from "@aws-amplify/backend"
 
-const schema = a.schema({
-  Todo: a
-    .model({
-      content: a.string(),
-      isDone: a.boolean()
-    })
-    .authorization(allow => [allow.owner()])
-});
-
-export type Schema = ClientSchema<typeof schema>;
-
-export const data = defineData({
-  schema,
-  authorizationModes: {
-    defaultAuthorizationMode: 'userPool'
-  }
-});
+/**
+ * Define and configure your auth resource
+ * @see https://docs.amplify.aws/gen2/build-a-backend/auth
+ */
+export const auth = defineAuth({
+  loginWith: {
+    email: true,
+  },
+})
