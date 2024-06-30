0bc43648068d4acd77d5a5cfd2518c1208da607c
Jonathan <jonathan.joe2745@gmail.com>
2024-06-30 10:19:57 +0800

    added storage backend

--- pages/Profile.tsx
+++ pages/Profile.tsx
@@ -1,7 +1,8 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useAuth } from "../src/hooks/useAuth.tsx";
 import { useNavigate } from "react-router-dom";
 // import { useLocalStorage } from "../src/hooks/useLocalStorage.tsx";
+import { uploadData } from "aws-amplify/storage";
 
 export const ProfilePage = () => {
   const { logout } = useAuth();
@@ -29,12 +30,31 @@ export const ProfilePage = () => {
     }
   }, [shouldNavigate, navigate]);
 
+  const [file, setFile] = React.useState();
+
+  const handleChange = (event: any) => {
+    setFile(event.target.files[0]);
+  };
+
+  
+
   return (
     <div>
       <h1>This is a Profile page</h1>
       <button onClick={handleLogout}>Logout</button>
       <button onClick={handleSecrets}>Secrets</button>
       <button onClick={handleHome}>Home</button>
+      <input type="file" onChange={handleChange} />
+        <button
+          onClick={() =>
+            uploadData({
+              path: `picture-submissions/${file.name}`,
+              data: file,
+          })
+        }
+      >
+        Upload
+      </button>
     </div>
   );
 };
