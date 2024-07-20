1cd4a01ca50e04d4270d8e3ed75cb2aee925eca4
Jonathan <jonathan.joe2745@gmail.com>
2024-07-21 00:47:17 +0800

    UI added

---
+++ src/components/MenuOptions.tsx
@@ -0,0 +1,15 @@
+import { Menu, MenuItem } from "@aws-amplify/ui-react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../hooks/useAuth";
+
+export const MenuOptions = () => {
+  const { logout } = useAuth();
+  const navigate = useNavigate();
+
+  return (
+    <Menu>
+      <MenuItem onClick={() => {navigate('/profile')}}>My Profile</MenuItem>
+      <MenuItem onClick={logout}>Logout</MenuItem>
+    </Menu>
+  );
+}
